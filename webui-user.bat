95b7c89c7ef9735462b1cf61f542b6bd2a5023df
千秋九 <131428984+yuno779@users.noreply.github.com>
2024-11-19 13:49:29 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,7 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--theme light --xformers --xformers-flash-attention --opt-channelslast --disable-nan-check --precision half
 
 @REM Uncomment following code to reference an existing A1111 checkout.
 @REM set A1111_HOME=Your A1111 checkout dir
